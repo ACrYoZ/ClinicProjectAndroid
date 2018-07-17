5ec33f4a6a9f5c7c72707b8706f1e71a2bb57b86
vladislav <mrtvzat2013@yandex.com>
2018-07-17 20:40:58 +0300

    Проект с Navigation drawer v1.1

--- app/src/main/java/com/clinic/myclinic/Activities/UserProfileActivity.java
+++ app/src/main/java/com/clinic/myclinic/Activities/UserProfileActivity.java
@@ -2,8 +2,13 @@ package com.clinic.myclinic.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,11 +19,17 @@ import com.squareup.picasso.Picasso;
 
 import es.dmoral.toasty.Toasty;
 
-public class UserProfileActivity extends AppCompatActivity {
+public class UserProfileActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    private Toolbar mToolbar;
 
     //Объявляем компоненты интерфейса
-    ImageView userPhoto;
-    TextView userName, userAge, userAdress, userDiagnosis, userMedication;
+    ImageView userPhoto, userPhotoNavigationDrawer;
+    TextView userName, userAge, userAdress, userDiagnosis, userMedication, userEmail, userNameNavigationDrawer;
 
 
     //Объявляем пользователя
@@ -50,10 +61,18 @@ public class UserProfileActivity extends AppCompatActivity {
                 "Haven't Medication"
         );
 
-        // Handle Toolbar
-        //final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        mToolbar = findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
 
+        mDrawerLayout = findViewById(R.id.drawer_layout_user_profile);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open_en, R.string.close_en);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Для профиля
         userPhoto = findViewById(R.id.imgUser);
         userName = findViewById(R.id.txtUserName);
         userAge = findViewById(R.id.txtAgeNum);
@@ -61,6 +80,10 @@ public class UserProfileActivity extends AppCompatActivity {
         userDiagnosis = findViewById(R.id.txtDiagnosisText);
         userMedication = findViewById(R.id.txtMedicationText);
 
+        //Для Navigation Drawer TODO: пофиксить. Ошибка с тем, что элементы находятся в другом layout. Как до них достучаться?
+        //userPhotoNavigationDrawer = findViewById(R.id.imgUserNavigationDrawer);
+        //userEmail = findViewById(R.id.txtUserEmailNavigationDrawer);
+        //userNameNavigationDrawer = findViewById(R.id.txtUserNameNavigationDrawer);
 
         //Устанавливаем картинку из интернета TODO: временное решение. Переделать чтобы грузилась с сервера
         Picasso.get()
@@ -68,6 +91,11 @@ public class UserProfileActivity extends AppCompatActivity {
                 .resize(100, 100)
                 .centerCrop()
                 .into(userPhoto);
+       // Picasso.get()
+       //         .load(user.getUserPhoto())
+       //         .resize(100, 100)
+       //         .centerCrop()
+       //         .into(userPhotoNavigationDrawer);
 
 
         userName.setText(user.getUserName() + " " + user.getUserPatronymic() + " " + user.getUserSurname());
@@ -75,6 +103,8 @@ public class UserProfileActivity extends AppCompatActivity {
         userAdress.setText(user.getUserAdress());
         userDiagnosis.setText(user.getUserDiagnosis());
         userMedication.setText(user.getUserMedication());
+//      userNameNavigationDrawer.setText(user.getUserName() + " " + user.getUserPatronymic() + " " + user.getUserSurname());
+//      userEmail.setText(user.getUserEmail());
     }//onCreate
 
     private void startRecordsActivity(){
@@ -94,4 +124,48 @@ public class UserProfileActivity extends AppCompatActivity {
         startActivity(login);
         finish();
     }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_my_account:
+                break;
+            case R.id.nav_my_schedules:
+                startRecordsActivity();
+            case R.id.nav_logout:
+                onLogout();
+            case R.id.nav_settings:
+                break;
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_user_profile);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_user_profile);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Установка возможности клика по "гамбургеру". Т.е. без этого действия, клик по "гамбургеру ничего делать не булет"
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }//if
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
